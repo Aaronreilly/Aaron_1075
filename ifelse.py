228f65e80491e7c46b87235efa2effee3df46e66
Aaronreilly <raaron@karunya.edu.in>
2025-03-18 23:03:08 +0530

    "There is a error"

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+movies="Frozen","FantasticBeast","JungleBook"
+money=input("enter the money you have:")
+if(money>=500):
+     print("Welcome to ",movies[0])
+elif (money>=250):
+     print("Welcome to ",movies[1])
+elif (money>=120):
+     print("Welcome to ",movies[2])
+else:
+     print("Pl go and watch ur Tele vision")
